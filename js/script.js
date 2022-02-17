c606b3969747c9625416a9558f84814c9fd43d7a
adritadeb <adritad2019@gmail.com>
2022-02-18 00:56:17 +0600

    mobile view

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-// error
-// function error(){
-
-// }
-
-// expense items input
+// expense items
 function getInput(ExpenseItem) {
-    // debugger;
+    // expense input
     const expenseInput = document.getElementById(ExpenseItem + '-expense');
     const expenseInputText = expenseInput.value;
     const expenseAmount = parseFloat(expenseInputText);
+    // errors
     const stringError = document.getElementById('string-error');
     const negativeNumberError = document.getElementById('negative-number-error');
     const savingsError = document.getElementById('savings-error');
     const expensesError = document.getElementById('expenses-error');
+    // through error
     if (isNaN(expenseInputText)) {
         stringError.style.display = 'block';
         negativeNumberError.style.display = 'none';
@@ -20,12 +17,18 @@ function getInput(ExpenseItem) {
         expensesError.style.display = 'none';
     }
     else if (expenseAmount < 0) {
-        negativeNumberError.style.display = 'block';
         stringError.style.display = 'none';
+        negativeNumberError.style.display = 'block';
         savingsError.style.display = 'none';
         expensesError.style.display = 'none';
     }
-    return expenseAmount;
+    else {
+        stringError.style.display = 'none';
+        negativeNumberError.style.display = 'none';
+        savingsError.style.display = 'none';
+        expensesError.style.display = 'none';
+        return expenseAmount;
+    }
 
 }
 // calculate total expenses
@@ -39,90 +42,82 @@ function calculateExpenses() {
 }
 // calculate total balance
 function calculateBalance() {
+    // income input
     const incomeInput = document.getElementById('income-input');
     const incomeInputText = incomeInput.value;
     const incomeAmount = parseFloat(incomeInputText);
+    // total expense
     const totalExpenses = document.getElementById('total-expenses');
     const totalExpensesAmount = totalExpenses.innerText;
-
+    //  errors
     const stringError = document.getElementById('string-error');
     const negativeNumberError = document.getElementById('negative-number-error');
     const savingsError = document.getElementById('savings-error');
     const expensesError = document.getElementById('expenses-error');
+    // through error
     if (isNaN(incomeInputText)) {
         stringError.style.display = 'block';
         negativeNumberError.style.display = 'none';
         savingsError.style.display = 'none';
         expensesError.style.display = 'none';
-
     }
     else if (incomeAmount < 0) {
         negativeNumberError.style.display = 'block';
         stringError.style.display = 'none';
         savingsError.style.display = 'none';
         expensesError.style.display = 'none';
-
     }
     else if (totalExpensesAmount > incomeAmount) {
         expensesError.style.display = 'block'
         negativeNumberError.style.display = 'none';
         stringError.style.display = 'none';
         savingsError.style.display = 'none';
-
     }
-
-    // total expenses amount
     else {
         negativeNumberError.style.display = 'none';
         stringError.style.display = 'none';
         savingsError.style.display = 'none';
         expensesError.style.display = 'none';
-
-
-        const totalExpenses = document.getElementById('total-expenses');
-        const totalExpensesAmount = totalExpenses.innerText;
-
+        // total balance
         const totalBalance = document.getElementById('total-balance');
         totalBalance.innerText = incomeAmount - totalExpensesAmount;
     }
 }
 //calculate savings
 function calculateSavings() {
-    // debugger;
+    // income input
     const incomeInput = document.getElementById('income-input');
     const incomeInputText = incomeInput.value;
     const incomeAmount = parseFloat(incomeInputText);
-
+    // total balance
     const totalBalance = document.getElementById('total-balance');
     const totalBalanceText = totalBalance.innerText;
     const totalBalanceAmount = parseFloat(totalBalanceText);
-
+    //  save input
     const saveInput = document.getElementById('save-input');
     let saveInputText = saveInput.value;
     const saveAmount = parseFloat(saveInputText);
-
+    // saving amount
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = (incomeAmount * saveAmount) / 100;
+    saveInputText = savingAmount.innerText;
+    // errors
     const stringError = document.getElementById('string-error');
     const negativeNumberError = document.getElementById('negative-number-error');
     const savingsError = document.getElementById('savings-error');
     const expensesError = document.getElementById('expenses-error');
-
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = (incomeAmount * saveAmount) / 100;
-    saveInputText = savingAmount.innerText;
-
+    // through error
     if (isNaN(saveInputText)) {
         stringError.style.display = 'block';
         negativeNumberError.style.display = 'none';
         expensesError.style.display = 'none';
         savingsError.style.display = 'none';
-
     }
     else if (saveAmount < 0) {
         negativeNumberError.style.display = 'block';
         stringError.style.display = 'none';
         expensesError.style.display = 'none';
         savingsError.style.display = 'none'
-
     }
     else if (totalBalanceAmount < saveInputText) {
         savingsError.style.display = 'block';
@@ -135,24 +130,17 @@ function calculateSavings() {
         stringError.style.display = 'none';
         savingsError.style.display = 'none';
         expensesError.style.display = 'none';
-
-        const savingAmount = document.getElementById('saving-amount');
-        savingAmount.innerText = (incomeAmount * saveAmount) / 100;
-
-        saveInputText = savingAmount.innerText;
         //calculate remaining balance
         const remainingBalance = document.getElementById('remaining-balance');
-        const totalBalance = document.getElementById('total-balance');
-        const totalBalanceText = totalBalance.innerText;
-        const totalBalanceAmount = parseFloat(totalBalanceText);
         remainingBalance.innerText = totalBalanceAmount - saveInputText;
     }
 }
-
+// calculate balance
 document.getElementById('calculate-button').addEventListener('click', function () {
     calculateExpenses();
     calculateBalance();
 })
+// calculate saving
 document.getElementById('save-button').addEventListener('click', function () {
     calculateSavings();
 })
